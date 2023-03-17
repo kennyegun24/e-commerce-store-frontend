60d83d19a383fc89f21be3293517942669ddec40
Kenny Egun <kennyegun240@gmail.com>
2023-03-17 17:34:13 +0100

    Modify

--- src/components/pages/Orders.jsx
+++ src/components/pages/Orders.jsx
@@ -1,48 +1,46 @@
 import React, { useEffect } from 'react'
-import img from '../../assets/vic.jpg'
 import { useDispatch, useSelector } from 'react-redux'
-import { getStore } from '../../redux/store/store'
 import { fetchOrders } from '../../redux/order/order'
 
 const Orders = () => {
-    const { orders } = useSelector((state) => state.order)
-    const { currentUser } = useSelector(state => state.user)
-    const dispatch = useDispatch()
+  const { orders } = useSelector((state) => state.order)
+  const { currentUser } = useSelector(state => state.user)
+  const dispatch = useDispatch()
 
-    useEffect(() => {
-        dispatch(fetchOrders(currentUser.data.token))
-    }, [])
+  useEffect(() => {
+    dispatch(fetchOrders(currentUser.data.token))
+  }, [])
 
-    return (
-        <div className='orderSection'>
-            <p className='orderTxt'>
-                Orders
-            </p>
-            <div>
-                {orders.map((indOrd, index) => (
-                    <div key={indOrd.id}>
-                        {index + 1}
+  return (
+    <div className='orderSection'>
+      <p className='orderTxt'>
+        Orders
+      </p>
+      <div>
+        {orders.map((indOrd, index) => (
+          <div key={indOrd.id}>
+            {index + 1}
 
-                        <aside className='asideImg'>
-                            <img src={indOrd.image} alt="" />
-                        </aside>
-                        <aside className='asideDetails'>
-                            <div>
-                                <p>{indOrd.user_name}</p>
-                                <p>{indOrd.address}</p>
-                                <p>{indOrd.tel_number}</p>
-                            </div>
+            <aside className='asideImg'>
+              <img src={indOrd.image} alt="" />
+            </aside>
+            <aside className='asideDetails'>
+              <div>
+                <p>{indOrd.user_name}</p>
+                <p>{indOrd.address}</p>
+                <p>{indOrd.tel_number}</p>
+              </div>
 
-                            <div>
-                                <p>{indOrd.product_name}</p>
-                                <p>Red</p>
-                                <p>{indOrd.quantity} pieces</p>
-                            </div>
-                        </aside>
-                    </div>
-                ))}
-            </div></div>
-    )
+              <div>
+                <p>{indOrd.product_name}</p>
+                <p>Red</p>
+                <p>{indOrd.quantity} pieces</p>
+              </div>
+            </aside>
+          </div>
+        ))}
+      </div></div>
+  )
 }
 
 export default Orders
