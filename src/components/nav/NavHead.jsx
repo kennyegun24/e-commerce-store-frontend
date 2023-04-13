540d5b661d93779e3ab57da68060feec7b174004
Kenny Egun <kennyegun240@gmail.com>
2023-04-13 21:26:26 +0100

    Create and style nav components

---
+++ src/components/nav/NavHead.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './nav.css'
+const NavHead = () => {
+  return (
+    <>
+      <nav className='navHeadMainNav'>
+        <h3>
+          Shop Spree
+        </h3>
+        <h5>
+          Store owners dashboard
+        </h5>
+      </nav>
+    </>
+  )
+}
+
+export default NavHead
