146117850ca93fe95d531783df5e17e5e430f08d
Kenny Egun <kennyegun240@gmail.com>
2023-04-16 01:23:58 +0100

    Created other components to render Tables and orders

---
+++ src/components/pages/Orders.jsx
@@ -0,0 +1,124 @@
+import React from 'react'
+import img from '../../assets/vic.jpg'
+
+const Orders = () => {
+    return (
+        <div className='orderSection'>
+            <p className='orderTxt'>
+                Orders
+            </p>
+            <div>
+
+                <div>
+                    <aside className='asideImg'>
+                        <img src={img} alt="" />
+                    </aside>
+                    <aside className='asideDetails'>
+                        <div>
+                            <p>Kenny Elias</p>
+                            <p>Block N18, Flat 3, Abesan Estate, Ipaja</p>
+                            <p>+234 802 546 4789</p>
+                        </div>
+
+                        <div>
+                            <p>Samsung Galaxy Fold</p>
+                            <p>Red</p>
+                            <p>2 pieces</p>
+                        </div>
+                    </aside>
+                </div>
+                <div>
+                    <aside className='asideImg'>
+                        <img src={img} alt="" />
+                    </aside>
+                    <aside className='asideDetails'>
+                        <div>
+                            <p>Kenny Elias</p>
+                            <p>Block N18, Flat 3, Abesan Estate, Ipaja</p>
+                            <p>+234 802 546 4789</p>
+                        </div>
+
+                        <div>
+                            <p>Samsung Galaxy Fold</p>
+                            <p>Red</p>
+                            <p>2 pieces</p>
+                        </div>
+                    </aside>
+                </div>
+                <div>
+                    <aside className='asideImg'>
+                        <img src={img} alt="" />
+                    </aside>
+                    <aside className='asideDetails'>
+                        <div>
+                            <p>Kenny Elias</p>
+                            <p>Block N18, Flat 3, Abesan Estate, Ipaja</p>
+                            <p>+234 802 546 4789</p>
+                        </div>
+
+                        <div>
+                            <p>Samsung Galaxy Fold</p>
+                            <p>Red</p>
+                            <p>2 pieces</p>
+                        </div>
+                    </aside>
+                </div>
+                <div>
+                    <aside className='asideImg'>
+                        <img src={img} alt="" />
+                    </aside>
+                    <aside className='asideDetails'>
+                        <div>
+                            <p>Kenny Elias</p>
+                            <p>Block N18, Flat 3, Abesan Estate, Ipaja</p>
+                            <p>+234 802 546 4789</p>
+                        </div>
+
+                        <div>
+                            <p>Samsung Galaxy Fold</p>
+                            <p>Red</p>
+                            <p>2 pieces</p>
+                        </div>
+                    </aside>
+                </div>
+                <div>
+                    <aside className='asideImg'>
+                        <img src={img} alt="" />
+                    </aside>
+                    <aside className='asideDetails'>
+                        <div>
+                            <p>Kenny Elias</p>
+                            <p>Block N18, Flat 3, Abesan Estate, Ipaja</p>
+                            <p>+234 802 546 4789</p>
+                        </div>
+
+                        <div>
+                            <p>Samsung Galaxy Fold</p>
+                            <p>Red</p>
+                            <p>2 pieces</p>
+                        </div>
+                    </aside>
+                </div>
+                <div>
+                    <aside className='asideImg'>
+                        <img src={img} alt="" />
+                    </aside>
+                    <aside className='asideDetails'>
+                        <div>
+                            <p>Kenny Elias</p>
+                            <p>Block N18, Flat 3, Abesan Estate, Ipaja</p>
+                            <p>+234 802 546 4789</p>
+                        </div>
+
+                        <div>
+                            <p>Samsung Galaxy Fold</p>
+                            <p>Red</p>
+                            <p>2 pieces</p>
+                        </div>
+                    </aside>
+                </div>
+            </div></div>
+    )
+}
+
+export default Orders
---
+++ src/components/pages/Table.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+import img from '../../assets/vic.jpg'
+const Table = () => {
+    const { products } = useSelector(state => state.store)
+    console.log(products)
+    return (
+        <div>
+            <table>
+                <tr className='tr'>
+                    <th>Product name</th>
+                    <th>Product price</th>
+                    <th>Image</th>
+                    <th>Items left</th>
+                </tr>
+
+                {products.map((prod) => (
+                    <tr className='trD' key={prod.id}>
+                        <td>{prod.name}</td>
+                        <td>{prod.price}</td>
+                        <img className='prodImage' src={img} />
+                        <td>{prod.in_stock}</td>
+                    </tr>
+                ))}
+            </table>
+        </div>
+    )
+}
+
+export default Table
