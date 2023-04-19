be09bf482d7b000c8e846829722df2873a396d86
Kenny Egun <kennyegun240@gmail.com>
2023-04-19 11:14:07 +0100

    Use material UI grid table to display orders and lists of products

--- src/components/pages/Orders.jsx
+++ src/components/pages/Orders.jsx
@@ -1,41 +1,48 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { DataGrid } from '@mui/x-data-grid';
+import { fetchOrders } from '../../redux/order/order';
 
 const Orders = () => {
   const { orders } = useSelector((state) => state.order)
+  const { currentUser } = useSelector(state => state.user)
+  const dispatch = useDispatch()
+  const ref = useRef(orders)
 
   useEffect(() => {
+    if (orders.length <= 0) {
+      dispatch(fetchOrders(currentUser.data.token))
+      ref.current = orders
+    }
   }, [])
 
+  const columns = [
+    { field: 'id', headerName: 'ID', width: 50 },
+    { field: 'user_name', headerName: 'Userame', width: 150 },
+    { field: 'product_name', headerName: 'Product name', width: 200 },
+    { field: 'address', headerName: 'Address', width: 300 },
+    { field: 'tel_number', headerName: 'Tel', width: 150 },
+    { field: 'quantity', headerName: 'Quantity', width: 70 },
+    {
+      field: 'image', headerName: 'Image',
+      renderCell: (params) => (
+        <img src={params.value} alt='' style={{ width: 50, borderRadius: 50 }} />
+      ),
+      sortable: false,
+      width: 120
+    }
+  ];
+
   return (
-    <div className='orderSection'>
-      <p className='orderTxt'>
-        Orders
-      </p>
-      <div>
-        {orders.map((indOrd, index) => (
-          <div key={indOrd.id}>
-            {index + 1}
-
-            <aside className='asideImg'>
-              <img src={indOrd.image} alt="" />
-            </aside>
-            <aside className='asideDetails'>
-              <div>
-                <p>{indOrd.user_name}</p>
-                <p>{indOrd.address}</p>
-                <p>{indOrd.tel_number}</p>
-              </div>
-
-              <div>
-                <p>{indOrd.product_name}</p>
-                <p>Red</p>
-                <p>{indOrd.quantity} pieces</p>
-              </div>
-            </aside>
-          </div>
-        ))}
-      </div></div>
+    <div style={{ height: '100%' }}>
+      <DataGrid
+        rows={orders}
+        columns={columns}
+        pageSize={5}
+        rowsPerPageOptions={[5]}
+        checkboxSelection
+      />
+    </div>
   )
 }
 
--- src/components/pages/Table.jsx
+++ src/components/pages/Table.jsx
@@ -1,27 +1,46 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
-import img from '../../assets/vic.jpg'
+import React, { useEffect, useRef } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+// import img from '../../assets/vic.jpg'
+import { DataGrid } from '@mui/x-data-grid';
+import { getStore } from '../../redux/store/store';
 const Table = () => {
     const { products } = useSelector(state => state.store)
-    return (
-        <div>
-            <table>
-                <tr className='tr'>
-                    <th>Product name</th>
-                    <th>Product price</th>
-                    <th>Image</th>
-                    <th>Items left</th>
-                </tr>
+    const { currentUser } = useSelector(state => state.user)
+    const dispatch = useDispatch()
+    const ref = useRef(products)
 
-                {products.map((prod) => (
-                    <tr className='trD' key={prod.id}>
-                        <td>{prod.name}</td>
-                        <td>{prod.price}</td>
-                        <img className='prodImage' src={prod.image} />
-                        <td>{prod.in_stock}</td>
-                    </tr>
-                ))}
-            </table>
+    useEffect(() => {
+        if (products.length <= 0) {
+            dispatch(getStore(currentUser.data.token))
+            ref.current = products
+        }
+    }, [])
+
+    const columns = [
+        { field: 'id', headerName: 'ID', width: 100 },
+        { field: 'name', headerName: 'Product Name', width: 300 },
+        { field: 'price', headerName: 'Price', width: 150 },
+        { field: 'in_stock', headerName: 'Quantity Left', width: 150 },
+
+        {
+            field: 'image', headerName: 'Image',
+            renderCell: (params) => (
+                <img src={params.value} alt='' style={{ width: 50, borderRadius: 50 }} />
+            ),
+            sortable: false,
+            width: 120
+        }
+    ];
+
+    return (
+        <div style={{ height: '100%', width: '100%' }}>
+            <DataGrid
+                rows={products}
+                columns={columns}
+                pageSize={5}
+                rowsPerPageOptions={5}
+                checkboxSelection
+            />
         </div>
     )
 }
