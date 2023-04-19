bf72f29c4ff955f956a880d90e1dd68ec12ee343
Kenny Egun <kennyegun240@gmail.com>
2023-04-19 11:10:38 +0100

    Modify

--- src/components/chart/LineChart.jsx
+++ src/components/chart/LineChart.jsx
@@ -2,9 +2,11 @@ import React from 'react'
 import { Line } from 'react-chartjs-2'
 import { Chart as ChartJs } from 'chart.js/auto'
 /* eslint-disable */
-const LineChart = ({ chartData }) => {
+const LineChart = ({ chartData, options }) => {
     return (
-        <Line data={chartData} />
+        <div style={{ width: '100%', height: '100%' }}>
+            <Line data={chartData} options={options} />
+        </div>
     )
 }
 
